8d93ea8b5e0925dbac7e179d79db3edca8443499
Dhiren <77841136+Dhirennn@users.noreply.github.com>
2025-01-05 02:40:54 +0800

    Add basic Game class

--- code/main.py
+++ code/main.py
@@ -1,40 +1,40 @@
 from settings import *
 from player import Player
+import sys
 
 class Game:
     def __init__(self):
-        # setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Survivor')
-        self.clock = pygame.time.Clock()
-        self.running = True
+        pygame.display.set_caption("Vampire Survivors Py")
+        self.clock =  pygame.time.Clock()
+        self.is_running = True
 
-        # groups 
+        # Groups
         self.all_sprites = pygame.sprite.Group()
 
-        # sprites
-        self.player = Player((400,300), self.all_sprites)
+        # Sprites
 
     def run(self):
-        while self.running:
-            # dt 
-            dt = self.clock.tick() / 1000
-
-            # event loop 
+        while self.is_running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.running = False
+                    self.is_running = False
+                    sys.exit()
 
-            # update 
-            self.all_sprites.update(dt)
+            # Update sprites
+            self.all_sprites.update()
 
-            # draw
-            self.display_surface.fill('black')
+            # Draw the game
+            self.display_surface.fill('Black')
             self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
+
         pygame.quit()
+
+
+
 
 if __name__ == '__main__':
     game = Game()
