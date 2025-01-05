26cfb15cd3a76d1bebfb2e08c04e476d39b1490e
Dhiren <77841136+Dhirennn@users.noreply.github.com>
2025-01-05 18:37:38 +0800

    small improvements

--- code/player.py
+++ code/player.py
@@ -3,9 +3,12 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
+        self.load_images()
         self.image = pygame.image.load(join('images','player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
-        self.hitbox_rect = self.rect.inflate(-70, -100)
+        self.hitbox_rect = self.rect.inflate(-70, -80)
+
+        self.state, self.frame_index = 'down', 0
 
 
         self.speed = 500
@@ -13,6 +16,33 @@ class Player(pygame.sprite.Sprite):
 
         # Groups
         self.collision_sprites = collision_sprites
+
+    def load_images(self):
+        self.frames = {'left': [], 'right': [], 'up': [], 'down': []}
+
+        for state in self.frames.keys():
+            for folder_path, _, file_names in walk(join('images','player', state)):
+                if file_names:
+                    for file_name in sorted(file_names, key = lambda name: int(name.split('.')[0])):
+                        full_path = join(folder_path, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
+
+
+    def animate(self, dt):
+
+        # state
+        if self.direction.x != 0:
+            self.state = 'right' if self.direction.x > 0 else 'left'
+
+        if self.direction.y != 0:
+            self.state = 'down' if self.direction.y > 0 else 'up'
+
+
+        # animation logic
+        self.frame_index = self.frame_index + 5 * dt if self.direction else 0
+        self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
+
 
     def key_input(self):
         keys = pygame.key.get_pressed()
@@ -62,3 +92,4 @@ class Player(pygame.sprite.Sprite):
     def update(self, dt):
         self.key_input()
         self.move(dt)
+        self.animate(dt)
